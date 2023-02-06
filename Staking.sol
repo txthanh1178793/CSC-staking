ed5521ff0175237f4a6662b2330eb198e008e229
ThanhTX <49052346+txthanh1178793@users.noreply.github.com>
2023-02-06 10:33:14 +0700

    Update Staking.sol

--- Staking.sol
+++ Staking.sol
@@ -228,6 +228,10 @@ contract LPStaking{
         require(owner == msg.sender, "Ownable: caller is not the owner");
         _;
     }
+    
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
 
     function openLPstaking() external onlyOwner{
         LPstakeable = true;
@@ -274,6 +278,41 @@ contract LPStaking{
     }
 }
 
+contract CetStakingReward{
+    IERC20 public token;
+    address public owner;
+    address public validator;
+
+    mapping(address => uint256) public reward;
+    
+    constructor(address _token, address _validator){
+        token = IERC20(_token);
+        owner = msg.sender;
+        validator = _validator;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    function claimReward() external{
+        reward[msg.sender] = 0;
+        require(token.transfer(msg.sender, reward[msg.sender]));        
+    }
+    function rewardLPDelivery(address[] memory stakers, uint[] memory balances, uint16 len, uint16 total) external onlyOwner{
+        uint totalReward = 100*10**token.decimals() ;
+        uint i = 0;
+        for(i = 0; i< len; i++){
+            reward[stakers[i]] = balances[i] * totalReward / total;
+        }
+
+    }
+}
 
 
 contract Vesting{
